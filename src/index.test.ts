0ef861a803513b3691b731b1d2c0c5c866406e5f
Marvin Hagemeister <hello@marvinh.dev>
2021-03-17 20:00:01 +0100

    Fix assert deprecation warning

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import * as k from './index';
-import * as t from 'assert';
+import { strict as t } from 'assert';
 import * as pty from 'node-pty';
 import * as path from 'path';
 import * as child_process from 'child_process';
