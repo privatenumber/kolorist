35826254cfa454b4d580cf03bbeb6824c14207c9
Hiroki Osame <osame@squareup.com>
2023-02-04 09:34:18 +0900

    build: use pkgroll

--- tools/post-build.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-
-const dir = path.join(__dirname, '..', 'dist', 'esm');
-fs.copyFileSync(path.join(dir, 'index.js'), path.join(dir, 'index.mjs'));
